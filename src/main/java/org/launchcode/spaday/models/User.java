43e1ee31080cfdb2394aa41902598c4d69d009de
Chance Goodwin <cgood1413@gmail.com>
2022-02-21 20:07:43 -0600

    Chapter 13 Studio Completion

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,26 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username is required.")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message = "Email must conform to proper email formatting.")
     private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
+    @NotNull(message = "Passwords must match")
+    @NotBlank(message = "Please confirm password.")
+    private String verifyPassword;
 
     public User() {
 
@@ -38,5 +55,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        this.checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        this.checkPassword();
+    }
+
+    private void checkPassword(){
+        if(!this.password.equals(verifyPassword)){
+            this.verifyPassword = null;
+        }
     }
 }
